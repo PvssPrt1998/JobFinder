2ebb071699f8c034d1aac7a174f8ca88ca66a123
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-04-19 19:52:42 +0500

    Initial Commit

---
+++ JobFinder/JobFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobFinderApp.swift
+//  JobFinder
+//
+//  Created by Николай Щербаков on 19.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct JobFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
